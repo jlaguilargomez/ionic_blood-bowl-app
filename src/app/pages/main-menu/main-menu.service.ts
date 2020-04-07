819427190f9e81955985866a9f77397770ca77fb
jlaguilargomez <jlaguigom@gmail.com>
2020-04-07 07:34:03 +0200

    new options added to the main-menu

--- src/app/pages/main-menu/main-menu.service.ts
+++ src/app/pages/main-menu/main-menu.service.ts
@@ -7,25 +7,35 @@ export interface MainBtnInterface {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MainMenuService {
   private btnContent: MainBtnInterface[] = [
     {
       img: 'assets/img/main-menu/teams_img.png',
       text: 'equipos',
-      link: '/teams'
+      link: '/teams',
     },
     {
       img: 'assets/img/main-menu/skills_img.png',
       text: 'habilidades',
-      link: '/skills'
+      link: '/skills',
     },
     {
       img: 'assets/img/main-menu/photos_img.png',
       text: 'fotos',
-      link: '/photos'
-    }
+      link: '/photos',
+    },
+    {
+      img: 'assets/img/main-menu/touchdown.jpg',
+      text: 'tablas',
+      link: '/boards',
+    },
+    {
+      img: 'assets/img/main-menu/resources_img.jpg',
+      text: 'recursos',
+      link: '/resources',
+    },
   ];
 
   constructor() {}
