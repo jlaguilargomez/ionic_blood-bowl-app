390512e14f343bdebca852bcf9fcb0f18bc360da
jlaguilargomez <jlaguigom@gmail.com>
2020-04-09 09:26:46 +0200

    teams

--- src/app/pages/teams/teams.page.ts
+++ src/app/pages/teams/teams.page.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from 'src/app/store.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.page.html',
-  styleUrls: ['./teams.page.scss']
+  styleUrls: ['./teams.page.scss'],
 })
 export class TeamsPage implements OnInit {
+  teamData: Array<{ name: string; img: string }>;
   ribbonText = 'Equipos';
-  constructor() {}
+  constructor(private store: Store) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.store.teamData$
+      .pipe(
+        map((res) =>
+          res.map((team) => {
+            return {
+              name: team.team,
+              img: team.logo,
+            };
+          })
+        )
+      )
+      .subscribe((res) => {
+        this.teamData = res;
+        console.log(this.teamData);
+      });
+  }
 }
